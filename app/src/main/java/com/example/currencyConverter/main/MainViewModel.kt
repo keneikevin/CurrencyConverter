f554195ef8e3cdab69ab33d1b3319f96170b7ab5
kenei <kevinkenei9@gmail.com>
2021-01-27 02:28:23 +0300

    ViewModel working

--- app/src/main/java/com/example/currencyConverter/main/MainViewModel.kt
+++ app/src/main/java/com/example/currencyConverter/main/MainViewModel.kt
@@ -2,14 +2,61 @@ package com.example.currencyConverter.main
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.currencyConverter.data.models.Rates
 import com.example.currencyConverter.util.DispatcherProvider
+import com.example.currencyConverter.util.Resource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlin.math.round
 
 class MainViewModel @ViewModelInject constructor(
 private val repository: MainRepository,
 private val dispatchers: DispatcherProvider
 
 ):ViewModel() {
+
+    sealed class CurrencyEvent{
+        class Success(val resultText:String): CurrencyEvent()
+        class Failure(val errorText:String): CurrencyEvent()
+        object Loading: CurrencyEvent()
+        object Empty: CurrencyEvent()
+    }
+
+    private val _conversion = MutableStateFlow<CurrencyEvent>(CurrencyEvent.Empty)
+    val conversion: StateFlow<CurrencyEvent> = _conversion
+
+    fun convert(
+        amountStr: String,
+        fromCurrency: String,
+        toCurrency: String
+    ) {
+        val fromAmount = amountStr.toFloatOrNull()
+        if(fromAmount == null) {
+            _conversion.value = CurrencyEvent.Failure("Not a valid amount")
+            return
+        }
+
+        viewModelScope.launch(dispatchers.io) {
+            _conversion.value = CurrencyEvent.Loading
+            when(val ratesResponse = repository.getRates(fromCurrency)) {
+                is Resource.Error -> _conversion.value = CurrencyEvent.Failure(ratesResponse.message!!)
+                is Resource.Success -> {
+                    val rates = ratesResponse.data!!.rates
+                    val rate = getRateForCurrency(toCurrency, rates)
+                    if(rate == null) {
+                        _conversion.value = CurrencyEvent.Failure("Unexpected error")
+                    } else {
+                        val convertedCurrency = round(fromAmount * rate * 100) / 100
+                        _conversion.value = CurrencyEvent.Success(
+                            "$fromAmount $fromCurrency = $convertedCurrency $toCurrency"
+                        )
+                    }
+                }
+            }
+        }
+    }
     private fun getRateForCurrency(currency: String, rates: Rates) = when (currency) {
         "CAD" -> rates.cAD
         "HKD" -> rates.hKD
